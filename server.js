6b93a3ee70423328a7219ec00f1a686994b90ae6
Hoshiyuki-Api <websitehoshiyuki@gmail.com>
2025-04-12 19:01:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const express = require('express');
-const path = require('path');
 const app = express();
+
+// Karena __dirname nggak ada di ESM, kamu bikin manual:
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'dist')));
